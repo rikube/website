2b98bf25a9bf7668fd971749bcc65fd345b4a9fb
Jason "Jay" Smith <jay@thejaysmith.com>
2019-09-22 20:01:12 -0700

    update to base with Dockerfile

--- main.py
+++ main.py
@@ -4,7 +4,12 @@ site = Flask(__name__)
 
 @site.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("pages/main.html")
+
+
+@site.route("/legal")
+def legal():
+    return render_template("pages/legal.html")
     
 @site.route("/salvador")
 def salvador():
